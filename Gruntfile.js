76d521ca82efe07f5ac31f7fc1e2c0f8eccbf0a9
Thomas Haukland <thomas.haukland@gmail.com>
2014-01-01 20:06:56 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+  require('grunt-load')(grunt).loadNpmTasks();
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      all: ['*.js', 'client/**/*.js', 'server/**/*.js', '!**/*.min.js']
+    },
+    less: {
+      dev: {
+        options: {
+          paths: ["client/css"],
+          cleancss: true
+        },
+        files: {
+          "client/css/bootstrap.css": "client/css/bootstrap.less"
+        }
+      }
+    },
+    cssmin: {
+      combine: {
+        files: {
+          'client/css/all.min.css': [
+            'client/css/bootstrap.css',
+            'client/css/custom.css'
+          ]
+        }
+      }
+    },
+    copy: {
+      dev: {
+        files: [
+          {expand: true, cwd: 'bower_components/bootstrap/dist', src: ['fonts/*'], dest: 'client/'},
+          {expand: true, cwd: 'bower_components/bootstrap/dist/js', src: ['*.min.js'], dest: 'client/js/lib/'},
+          {expand: true, cwd: 'bower_components/jquery/', src: ['jquery.min.js'], dest: 'client/js/lib/'}
+        ]
+      },
+      build: {
+        files: [
+          {expand: true, cwd: 'server/', src: ['**'], dest: 'build/server/'},
+          {expand: true, cwd: 'client', src: ['**/*.min.*', '*.html', 'fonts/*'], dest: 'build/client'}
+        ]
+      }
+    },
+    clean: ['build/']
+  });
+
+  // Default task(s).
+  grunt.registerTask('default', ['clean', 'jshint', 'less', 'cssmin', 'copy']);
+};
