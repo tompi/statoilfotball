455b78cdf58f1e3f0534a9a3a039755a148165a0
Thomas Haukland <thomas.haukland@gmail.com>
2014-02-09 15:13:57 +0100

    new domain

--- server/config.js
+++ server/config.js
@@ -11,7 +11,7 @@ module.exports = {
       clientID: '198166778757.apps.googleusercontent.com',
       clientSecret: 'f_d8LFCyqg1tyqFQ41xbOTTc',
       emailAddress: '198166778757@developer.gserviceaccount.com',
-      callbackURL: 'http://statoilfotball.herokuapp.com/oauth2callback'
+      callbackURL: 'http://fotball.us/oauth2callback'
     },
     /*
     facebook: {
@@ -24,7 +24,7 @@ module.exports = {
     facebook: {
       clientID: '403518086450774',
       clientSecret: '7b5399bdb5fff8970deb062bd150823b',
-      callbackURL: 'http://statoilfotball.herokuapp.com/auth/facebook/callback',
+      callbackURL: 'http://fotball.us/auth/facebook/callback',
       profileFields: ['id', 'name', 'emails', 'displayName', 'photos']
     }
   }
