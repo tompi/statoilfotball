2e40413985a8b84b16b9c8c4de85581215bef287
Thomas Haukland <thomas.haukland@gmail.com>
2016-04-26 10:35:50 +0200

    Prod config

--- server/config.js
+++ server/config.js
@@ -9,13 +9,14 @@ module.exports = {
   },
   sessionSecret: 'webstepsecret',
   passport: {
+    /*
     google: {
       clientID: '1098607882040.apps.googleusercontent.com',
       clientSecret: 'CISVtnCf9N5L7IZPBVddeK48',
       callbackURL: 'http://localhost:3000/oauth2callback',
       handler: '/oauth2callback'
-    },
-    /* Prod:
+    },*/
+    /* Prod: */
     google: {
       clientID: '198166778757.apps.googleusercontent.com',
       clientSecret: 'f_d8LFCyqg1tyqFQ41xbOTTc',
@@ -23,22 +24,19 @@ module.exports = {
       callbackURL: 'http://fotball.us/oauth2callback',
       handler: '/oauth2callback'
     },
-    */
     /* Debug:
-    */
     facebook: {
       clientID: '396026930541318',
       clientSecret: 'c8c711ef1c55b53b5ac926893c954d85',
       callbackURL: 'http://localhost:3000/auth/facebook/callback',
       profileFields: ['id', 'name', 'emails', 'displayName', 'photos']
-    }
-    /* Prod:
+    } */
+    /* Prod: */
     facebook: {
       clientID: '403518086450774',
       clientSecret: '7b5399bdb5fff8970deb062bd150823b',
       callbackURL: 'http://fotball.us/auth/facebook/callback',
       profileFields: ['id', 'name', 'emails', 'displayName', 'photos']
     }
-    */
   }
 };
