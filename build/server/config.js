1fdcea0d2eca30a4be1155e5ad9836806b3fc6bd
Thomas Haukland <thomas.haukland@gmail.com>
2016-11-12 09:53:16 +0100

    Built app this time...

--- build/server/config.js
+++ build/server/config.js
@@ -1,9 +1,5 @@
-var url = require('url');
-
-var production = typeof(NODE_ENV) !== 'undefined' && NODE_ENV === 'production';
-
-var  passportConfig = {
-    debug: {
+/*
+  debug: {
       google: {
         clientID: '1098607882040.apps.googleusercontent.com',
         clientSecret: 'CISVtnCf9N5L7IZPBVddeK48',
@@ -17,23 +13,9 @@ var  passportConfig = {
         profileFields: ['id', 'name', 'emails', 'displayName', 'photos']
       }
     },
-    production: {
-      google: {
-        clientID: '198166778757.apps.googleusercontent.com',
-        clientSecret: 'f_d8LFCyqg1tyqFQ41xbOTTc',
-        emailAddress: '198166778757@developer.gserviceaccount.com',
-        callbackURL: 'http://fotball.us/oauth2callback',
-        handler: '/oauth2callback'
-      },
-      facebook: {
-        clientID: '403518086450774',
-        clientSecret: '7b5399bdb5fff8970deb062bd150823b',
-        callbackURL: 'http://fotball.us/auth/facebook/callback',
-        profileFields: ['id', 'name', 'emails', 'displayName', 'photos']
-      }
     }
   };
-
+*/
 
 module.exports = {
   mongodb: {
@@ -43,5 +25,19 @@ module.exports = {
     password: 'topsecret'
   },
   sessionSecret: 'webstepsecret',
-  passport: production ? passportConfig.production : passportConfig.debug
+  passport: {
+    google: {
+      clientID: '198166778757.apps.googleusercontent.com',
+      clientSecret: 'f_d8LFCyqg1tyqFQ41xbOTTc',
+      emailAddress: '198166778757@developer.gserviceaccount.com',
+      callbackURL: 'http://fotball.us/oauth2callback',
+      handler: '/oauth2callback'
+    },
+    facebook: {
+      clientID: '403518086450774',
+      clientSecret: '7b5399bdb5fff8970deb062bd150823b',
+      callbackURL: 'http://fotball.us/auth/facebook/callback',
+      profileFields: ['id', 'name', 'emails', 'displayName', 'photos']
+    }
+  }
 };
